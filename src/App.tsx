80d684a8352eba7a99576b1742c753c6a068f56a
Fabiano <fabianhatori@gmail.com>
2020-08-12 01:13:05 -0300

    Commit inicial de projeto em React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// Components
+import QuestionCard from './components/QuestionCard'
+
+const App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.mouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div className='App'>
+        <h1>Jogo de Perguntas e respostas em REact</h1>
+          <button className="start" onclick={startTrivia}>
+            Começar
+          </button>
+          <p className="score"> Pontuação:</p>
+          <p>Carregar questão...</p>
+
+          <QuestionCard />
+          <button className="next" onclick={nextQuestion}>
+            Próxima pergunta
+          </button>
+     </div>
+   );
 }
 
 export default App;
